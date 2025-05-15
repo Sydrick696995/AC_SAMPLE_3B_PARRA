835f4273dc4acbfd4c6ff68eff14653b23f0d7a3
Sydrick Parra <syparra@my.cspc.edu.ph>
2025-05-15 02:33:02 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app - SYDRICK B. PARRA")
+st.title("🎈 My new app - SYDRICK B. PARRA HI HELLO WOWOW")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
